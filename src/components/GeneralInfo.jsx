a59a3da07ac23477f7ff4aa7829b93009c300d4b
H4ROOON <haroonadeel69@gmail.com>
2025-09-27 16:53:57 +0500

    add GeneralIndo components with editable form and display mode

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,3 +1,26 @@
+import {useState} from "react";
 export default function GeneralInfo() {
-  return <h2>General Info</h2>
+  
+  const [isEditing, setIsEditing]= useState(true);
+    return
+   <form action="#" method="get">
+
+         <div>
+                    <label for="Name">Name </label>
+                    <input type="text" id="Name" name="Name" required placeholder="Haroon Adeel" />
+                </div>
+                <div>
+                    <label for="Phone">Phone </label>
+                    <input type="tel" id="Phone" name="Phone" required placeholder="0312-3456789" />
+                </div>
+                <div>
+                    <label for="email">EMAIL </label>
+                    <input type="email" id="email" name="email" required placeholder="yourname@haroon.com" />
+                </div>
+                <div>
+                    <button>Submit</button>
+                    <button>Edit</button>
+                </div>
+  </form>
+
 }
