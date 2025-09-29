a35d09de61cc4d62efa14d73e32aff28570c23f6
H4ROOON <haroonadeel69@gmail.com>
2025-09-29 08:55:13 +0500

    Add PDF export functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import "./App.css";
-
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 function App() {
+  const cvRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    contentRef: cvRef,
+    documentTitle: "Haroon-CV",
+  });
+
   return (
     <div className="cv-page">
       <div className="cv-container">
@@ -15,31 +23,41 @@ function App() {
 
         <hr className="cv-hr" />
 
-        <section className="section">
-          <h2>General Info</h2>
-          <div className="section-content">
-            <GeneralInfo />
-          </div>
-        </section>
+        {/* Printable content */}
+        <div ref={cvRef}>
+          <section className="section">
+            <h2>General Info</h2>
+            <div className="section-content">
+              <GeneralInfo />
+            </div>
+          </section>
 
-        <section className="section">
-          <h2>Education</h2>
-          <div className="section-content">
-            <Education />
-          </div>
-        </section>
+          <section className="section">
+            <h2>Education</h2>
+            <div className="section-content">
+              <Education />
+            </div>
+          </section>
 
-        <section className="section">
-          <h2>Experience</h2>
-          <div className="section-content">
-            <Experience />
-          </div>
-        </section>
+          <section className="section">
+            <h2>Experience</h2>
+            <div className="section-content">
+              <Experience />
+            </div>
+          </section>
 
-        <section className="section">
-          <h2>Skills</h2>
-          <div className="section-content">
-            <Skills />
+          <section className="section">
+            <h2>Skills</h2>
+            <div className="section-content">
+              <Skills />
+            </div>
+          </section>
+        </div>
+        <section className="app">
+          <div className="actions">
+            <button className="btn primary" onClick={handlePrint}>
+              Download PDF
+            </button>
           </div>
         </section>
       </div>
